0e80777fc1a48cbe06ba77a99a5d7e9094bee9c7
wangxc <djun100@qq.com>
2014-11-21 16:29:46 +0800

    UCDetector 设置为只显示class，将无用类移除

--- src/com/cy/ResourceRemover.java
+++ src/com/cy/ResourceRemover.java
@@ -5,15 +5,20 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-/**F:\android\android-sdk\tools>lint --check "UnusedResources" F:\projects\android2\
- * umsqmf2g-2.2.0_develop > result.txt
+/**cn.sunyard.encrypt.Base64.<init>(Base64.java:3)  Class "Base64" has 0 references Base64  org.ucdetector.analyzeMarkerReference 
+ * cn.sunyard.model.AppModel.<init>(AppModel.java:66) Interface "AppModel.Observer" has 0 references  AppModel.Observer org.ucdetector.analyzeMarkerReference 
+ * com.chinaums.pospassport.POSPassportPayActivity.<init>(POSPassportPayActivity.java:314)  Member class "POSPassportPayActivity.POSPassportBindCardItemInfo" has 0 references  POSPassportPayActivity.POSPassportBindCardItemInfo  org.ucdetector.analyzeMarkerReference Xuechao Wang <br/>
+ * com.sunyard.chinaums.common.cons.UmsConstant.<init>(UmsConstant.java:210)  Enum "UmsConstant.OrderFunctionTagTable" has 0 references UmsConstant.OrderFunctionTagTable org.ucdetector.analyzeMarkerReference 
+ *com.sunyard.chinaums.common.cons.UmsConstant.<init>(UmsConstant.java:49)  Interface "UmsConstant.SaleType" has 0 references UmsConstant.SaleType  org.ucdetector.analyzeMarkerReference 
  * @author wangxc <br/>
  */
 public class ResourceRemover {
     private static final String TAG = ResourceRemover.class.getName();
     
-    static final String pathResult="F:/android/android-sdk/tools/result.txt";
+    static final String pathResult="F:/projects/projects-ecilpse_android/ucdetector_reports/UCDetectorReport_003.txt";
 
     
     public static void main(String[] args){
@@ -22,26 +27,42 @@ public class ResourceRemover {
             BufferedReader reader = new BufferedReader(new FileReader(pathResult));
             String line;
             int count = 0;
+            int count_innerClass=0;
+            int count_class=0;
             while((line = reader.readLine()) != null) {
-                if (line.contains("UnusedResources") && !line.contains("res\\values") && !line.contains("appcompat")) {
-                    count++;
-                    int end = line.indexOf(":");
+                count++;
+                if(isInnerClass(line)) {
+                    count_innerClass++;
+                    System.out.println("inner class:"+line);
+                    continue;
+                }
+                
+                if (line.contains("has 0 references")) {
+                    count_class++;
+                    int end = line.indexOf("<");
                     if (end != -1){
-                        String file = line.substring(0, end);
-                        String f = pathProject +"/"+file;
+                        line = line.substring(0, end-1);
+                        line=line.replace(".", "/");
+                        String f = pathProject +"/src/"+line+".java";
                         System.out.println(f);
                         new File(f).delete();
                     }
                 }
             }
             reader.close();
-            
+            System.out.println("count:"+count+"  count_innerClass:"+count_innerClass+"  count_class:"+count_class);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }        
     }
-
+    /**
+     */
+    private static boolean isInnerClass(String line){
+        Pattern pattern = Pattern.compile("\"[a-zA-Z_0-9]+\\.[a-zA-Z_0-9]+\" has 0 references");// \\.→\.字符串内\代表转义
+        Matcher matcher = pattern.matcher(line);
+        return matcher.find();//部分匹配，matcher.matches()为全匹配
+    }
 }
 
